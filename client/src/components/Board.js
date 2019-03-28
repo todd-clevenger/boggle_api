60571906166bee9f0064cb7937c3dfc2dea30470
Todd Clevenger <todcle@hotmail.com>
2019-03-27 20:45:55 -0700

    Adds Initial Layout
    Initial layout of board.

---
+++ client/src/components/Board.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './Board.css';
+
+export default class Board extends React.Component {
+	render() {
+		return (
+		    <div id="board">
+					<div class="row">
+        		<div class="boggle">
+            	<span>A</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>B</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>C</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>D</span>
+        		</div> 
+    			</div>
+					<div class="row">
+        		<div class="boggle">
+            	<span>A</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>B</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>C</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>D</span>
+        		</div> 
+    			</div>
+					<div class="row">
+        		<div class="boggle">
+            	<span>A</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>B</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>C</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>D</span>
+        		</div> 
+    			</div>
+					<div class="row">
+        		<div class="boggle">
+            	<span>A</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>B</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>C</span>
+        		</div> 
+        		<div class="boggle">
+            	<span>D</span>
+        		</div> 
+    			</div>
+				</div>
+		);
+	}
+}
